382887fe26365b2624e0a4a95dcc15678be94597
Nick Waelkens <hi@nick.plus>
2019-07-15 11:01:35 +0200

    :fire: Remove unused alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,5 +8,3 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
-
-alias copypp="cp -a $PROJECTS/teamleader/project-planning/dist/. $PROJECTS/teamleader/core/public_html/project_planning"
