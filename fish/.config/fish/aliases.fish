da69bfbf24db411f28d75f92a7ab9008c080f554
Nick Waelkens <hi@nick.plus>
2019-12-04 20:44:09 +0100

    :boom: Super simple fish setup

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,7 @@
+# Shortcuts
+alias reload! "source ~/.config/fish/config.fish"
+alias ... "cd ../.."
+alias dl "cd ~/Downloads"
+alias dt "cd ~/Desktop"
+alias c "cd ~/code"
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
