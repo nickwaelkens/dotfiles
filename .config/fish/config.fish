2c055a079c00d0e1731ea3344928980fdc246639
Nick Waelkens <hi@nick.plus>
2022-03-26 20:31:13 +0100

    :wrench: Run `jump` on session start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,8 @@ source ~/.config/fish/variables.fish
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/path.fish
 
+# https://github.com/gsamokovarov/jump
+jump shell fish | source
+
+# https://github.com/starship/starship
 starship init fish | source
