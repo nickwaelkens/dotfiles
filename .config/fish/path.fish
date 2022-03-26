3008993e9a4d44d380bb633896b520485a6e0667
Nick Waelkens <hi@nick.plus>
2022-03-26 15:32:21 +0100

    :wrench: Add Homebrew to $PATH
    Signed-off-by: Nick Waelkens <hi@nick.plus>

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,1 +1,3 @@
-fish_add_path -g /usr/local/sbin ~/Library/Python/3.8/bin
+fish_add_path -g /usr/local/sbin # binaries that require su privileges
+fish_add_path -g ~/Library/Python/3.8/bin # Python 3 binaries
+fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin # Homebrew
