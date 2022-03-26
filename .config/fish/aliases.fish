ae99ad9e1a4d4b68ded3e72f7a7ee730f49dddff
Nick Waelkens <hi@nick.plus>
2022-03-26 18:46:33 +0100

    :sparkles: Add show/hide aliases that respectively show/hide hidden files on macOS

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -6,3 +6,7 @@ alias dt "cd ~/Desktop"
 alias c "cd ~/code"
 alias storm="open -a (mdfind -name 'kMDItemFSName==\"*.app\"' -onlyin /Applications/ | grep 'WebStorm.')"
 alias pubkey="pbcopy < ~/.ssh/id_ed25519.pub | echo '=> Public key copied to pasteboard.'"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
