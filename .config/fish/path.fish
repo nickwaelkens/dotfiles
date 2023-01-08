a21935e54e7cc7c90bb2d89379eaa7ead238bb95
Nick Waelkens <hi@nick.plus>
2023-01-08 17:17:48 +0100

    :truck: Xcode-select ships with Python 3.9

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -1,3 +1,3 @@
 fish_add_path -g /usr/local/sbin # binaries that require su privileges
-fish_add_path -g ~/Library/Python/3.8/bin # Python 3 binaries
+fish_add_path -g ~/Library/Python/3.9/bin # Python 3 binaries
 fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin # Homebrew
